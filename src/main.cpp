ddce04a13204a519190d1c99b0cb272debe88d6d
Maximo <maximogalizia@gmail.com>
2026-03-09 14:59:55 -0300

    Guardado 2 del testeo de wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
-#include <Arduino.h>
+#include <Arduino.h>  //librerias de arduino
+#include <WiFi.h>   //La librería oficial para manejar el stack de red del ESP32.
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "TU_RED";     // Puntero a una cadena de caracteres con el nombre del Wi-Fi.
+const char* password = "TU_PASS"; // Contraseña de la red.
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200); // Inicia el puerto serie.
+  delay(2000);          // Delay de cortesía para que el USB CDC de la PC se estabilice.
+
+  WiFi.mode(WIFI_STA);  // Configura el chip como "Station". 
+                        // El ESP32 puede ser AP (crear red) o STA (conectarse a una).
+  
+  WiFi.begin(ssid, password); // Intenta el "handshake" con el router/gateway.
+
+  // Este es un bucle de bloqueo positivo
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print("."); // Imprime puntos hasta que el estado cambie a "WL_CONNECTED".
+  }
+
+  Serial.println(WiFi.localIP()); // Una vez conectado, el router nos da una IP local.
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Verificación de persistencia de red
+  if (WiFi.status() != WL_CONNECTED) {
+    // Si el router se apaga o hay interferencia, entramos aquí.
+    Serial.println("Reconectando...");
+    WiFi.disconnect(); // Limpiamos el estado previo.
+    WiFi.reconnect();  // Intentamos reenganchar la sesión.
+    
+    delay(5000); // Esperamos 5 segundos antes de reintentar para no bloquear el procesador.
+  }
 }
